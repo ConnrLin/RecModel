daedd3e782741f3ea8e48bab7ee178ac5159f600
stervt <70164733+stervt@users.noreply.github.com>
2023-04-08 14:14:01 -0700

    second commit

--- Transformer/attention/single_head_attention.py
+++ Transformer/attention/single_head_attention.py
@@ -1,3 +1,10 @@
+'''
+Author: Felix
+Date: 2023-04-03 20:27:44
+LastEditors: Felix
+LastEditTime: 2023-04-05 16:30:46
+Description: Please enter description
+'''
 import torch.nn as nn
 import torch.nn.functional as F
 import torch
@@ -8,16 +15,15 @@ Single head attention
 """
 class SingleHeadAttention(nn.Module):
     
-
     '''
     Author: Felix
     description: Calculate attention for given K,Q,V
-    param {*} K:Key ,shape:[batch_size,seq_len,d_model]
-    param {*} Q:Query shape:[batch_size,seq_len,d_model]
-    param {*} V:Value shape:[batch_size,seq_len,d_model]
-    param {*} mask
-    param {*} dropout
-    return {*} Softmax([K*Q.T/(d^0.5)])*V
+    param {torch.Tensor} K:Key ,shape:[batch_size,seq_len,d_model]
+    param {torch.Tensor} Q:Query shape:[batch_size,seq_len,d_model]
+    param {torch.Tensor} V:Value shape:[batch_size,seq_len,d_model]
+    param {torch.Tensor} mask
+    param {float} dropout
+    return {torch.Tensor} Softmax([K*Q.T/(d^0.5)])*V
     '''    
     def forward(K, Q, V,mask=None, dropout=None):
         score = torch.matmul(Q,K.transpose(-2, -1)) / math.sqrt(K.size[-1])
