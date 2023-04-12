198a167a4d1cd49798136ae081b56bbd01d3c35e
stervt <70164733+stervt@users.noreply.github.com>
2023-04-11 19:43:02 -0700

    change model

--- Transformer/__init__.py
+++ Transformer/__init__.py
@@ -1,3 +1,11 @@
-from .bert import BERTModel
+'''
+Author: Felix
+Date: 2023-04-11 14:52:07
+LastEditors: Felix
+LastEditTime: 2023-04-11 19:23:29
+Description: Please enter description
+'''
+# from .bert import BERTModel
 from .attention import MultiHeadAttention
 from .utils import Trainer
+from .models import BERTModel
---
+++ Transformer/models/bert_modules/__init__.py
@@ -0,0 +1,9 @@
+'''
+Author: Felix
+Date: 2023-04-11 19:19:09
+LastEditors: Felix
+LastEditTime: 2023-04-11 19:29:07
+Description: Please enter description
+'''
+from .bert import BERT
+from .embedding import BERTEmbedding
