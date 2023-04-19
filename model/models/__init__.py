cb3327e85deb853325e027369665ba6df31c3f64
stervt <70164733+stervt@users.noreply.github.com>
2023-04-18 20:52:24 -0700

    final commit

--- model/models/__init__.py
+++ model/models/__init__.py
@@ -1,14 +1,8 @@
+'''
+Author: Felix
+Date: 2023-04-18 20:00:38
+LastEditors: Felix
+LastEditTime: 2023-04-18 20:51:51
+Description: Please enter description
+'''
 from .bert import BERTModel
-from .dae import DAEModel
-from .vae import VAEModel
-
-# MODELS = {
-#     BERTModel.code(): BERTModel,
-#     DAEModel.code(): DAEModel,
-#     VAEModel.code(): VAEModel
-# }
-
-
-# def model_factory(args):
-#     model = MODELS[args.model_code]
-#     return model(args)
